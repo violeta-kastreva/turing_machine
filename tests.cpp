23f332a0a41aad9392acc35a2e404ea9f06c6ae7
Violeta Kastreva <81370278+violeta-kastreva@users.noreply.github.com>
2024-01-09 21:30:18 +0200

    modified tests

--- tests.cpp
+++ tests.cpp
@@ -6,35 +6,51 @@
 #include "turingmachine/machines/conditionalturingmachine.h"
 #include "turingmachine/machines/iterationturingmachine.h"
 
+std::string readFirstLine(const std::string& filename) {
+    std::ifstream file(filename);
+    std::string firstLine;
+    if (file.is_open()) {
+        std::getline(file, firstLine);
+    }
+    return firstLine;
+}
 
 TEST_CASE("Testing Factory Turing Machine") {
     auto* factory = new TuringMachineFactory();
     auto tm = factory->getMachine("../testFiles/regular.txt");
     tm->run("../testFiles/regular_output.txt");
+
+    std::string expectedOutput = ">1001 ";
+    REQUIRE(readFirstLine("../testFiles/regular_output.txt") == expectedOutput);
 }
-//
-//TEST_CASE("Testing Composition Turing Machine") {
-//    CompositionTuringMachine tm("../testFiles/composition2.txt");
-//    tm.run("../testFiles/composition_output.txt");
-//}
 
 TEST_CASE("Testing Factory Composition Turing Machine") {
     auto* factory = new TuringMachineFactory();
     auto tm = factory->getMachine("../testFiles/composition2.txt");
     tm->run("../testFiles/composition_output_factory.txt");
+
+    std::string expectedOutput = ">1001 ";
+    REQUIRE(readFirstLine("../testFiles/composition_output_factory.txt") == expectedOutput);
 }
 
 TEST_CASE("Testing Conditional Composition Turing Machine") {
     auto* factory = new TuringMachineFactory();
     auto tm = factory->getMachine("../testFiles/conditional.txt");
     tm->run("../testFiles/conditional_output.txt");
+
+    std::string expectedOutput = ">1 10";
+    REQUIRE(readFirstLine("../testFiles/conditional_output.txt") == expectedOutput);
 }
 
 TEST_CASE("Testing Loop Turing Machine") {
     auto* factory = new TuringMachineFactory();
     auto tm = factory->getMachine("../testFiles/loop.txt");
     tm->run("../testFiles/loop_output.txt");
+
+    std::string expectedOutput = ">01010 ";
+    REQUIRE(readFirstLine("../testFiles/loop_output.txt") == expectedOutput);
 }
+
 
 //TEST_CASE("Testing Multitape Turing Machine") {
 //    auto* factory = new TuringMachineFactory();
